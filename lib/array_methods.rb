a12d017b7b4ffef62207372822369385e94275ed
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-14 17:15:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  x= 0
-  array.length.times{|index| x=array[index] if array[index] < x}
-  x
+x=array[0]
+array.length.times do |index|
+  if array [index] <x 
+    x=array[index]
+  end
+end
+x 
 end
